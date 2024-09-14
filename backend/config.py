ffcfdd16bd9c257ace7934ad176eb407bf2b6d7c
Elijah <elijahamadi18@gmail.com>
2024-09-14 12:00:28 +0000

    removed backend

--- backend/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "your_openai_api_key")
-    UPLOAD_FOLDER = 'uploads/'
-    PROCESSED_FOLDER = 'processed/'
-
-config = Config()
--- backend/routes/events.py
+++
@@ -1,9 +0,0 @@
-import time
-import json
-
-processing_status = {"progress": 0}
-
-def status_stream():
-    while True:
-        time.sleep(1)
-        yield f"data: {json.dumps(processing_status)}\n\n"
